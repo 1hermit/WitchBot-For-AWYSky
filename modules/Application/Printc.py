89f14117d4d59f51226a6c7212cca319259af8a2
浅羽筱夕 <kirastar_shannei@163.com>
2023-10-02 22:44:53 +0800

    删除颜色

--- modules/Application/Printc.py
+++ modules/Application/Printc.py
@@ -1,18 +1,16 @@
 import datetime
-from colorama import init
 
 
 def Printc(string, printType="error"):  # 函数:打印带提示信息和颜色的文字
-    init(autoreset=True)
     nowTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 获取当前时间
     printType = printType.lower()
     if printType == "e":
-        print("[" + nowTime + "]\033[1;31m[WitchBot 错误]\033[0m " + string)
+        print("[" + nowTime + "][WitchBot 错误] " + string)
     elif printType == "w":
-        print("[" + nowTime + "]\033[1;33m[WitchBot 警告]\033[0m " + string)
+        print("[" + nowTime + "][WitchBot 警告] " + string)
     elif printType == "i":
-        print("[" + nowTime + "]\033[1;32m[WitchBot 信息]\033[0m " + string)
+        print("[" + nowTime + "][WitchBot 信息] " + string)
     elif printType == "d":
-        print("[" + nowTime + "]\033[1;36m[WitchBot 调试]\033[0m " + string)
+        print("[" + nowTime + "][WitchBot 调试] " + string)
     else:
-        print("[" + nowTime + "]\033[1;32m[WitchBot 信息]\033[0m " + string)
+        print("[" + nowTime + "][WitchBot 信息] " + string)
